cc561b3bb294529590be24e58fec7a77c6eae7c7
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2022-10-25 17:15:27 +0300

    Initial commit

---
+++ module2hw1/module2hw1/Actions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2hw1
+{
+    public class Actions
+    {
+        public static Result InfoMethod()
+        {
+            Logger.GetInstance().AddLog(Type.Info, "Start method: " + new StackFrame().GetMethod().Name.ToString());
+            return new Result(true);
+        }
+
+        public static Result WarningMethod()
+        {
+            Logger.GetInstance().AddLog(Type.Warning, "Skipped logic in method: " + new StackFrame().GetMethod().Name.ToString());
+            return new Result(true);
+        }
+
+        public static Result ErrorMethod()
+        {
+            return new Result(false, "I broke a logic.");
+        }
+    }
+}
